14bcd8608de1f4b866f312257b5893e6da4d4120
federico-murua <federicog.murua12@gmail.com>
2026-01-25 14:58:52 -0300

    avanzando con la interfaz

--- javascript/productos.js
+++ javascript/productos.js
@@ -1,16 +1,107 @@
 const productos = [
   {
     id: 1,
-    nombre: "Ensalada César",
-    precio: 4500,
-    descripcion: "Lechuga, pollo grillado, queso parmesano y croutons",
+    nombre: "Poke Teriyaki Fresh",
+    precio: 8500,
+    descripcion: "Pollo teriyaki, Arros de sushi, Zanahoria,\nLechuga, Repollo morado,Tomates\ncherrys, Semillas de sesamo",
     imagen: "./assets/img/salad1.jpg"
   },
   {
     id: 2,
-    nombre: "Ensalada Mediterránea",
-    precio: 4800,
-    descripcion: "Tomate, aceitunas, queso, rúcula y aceite de oliva",
+    nombre: "Poke Cream",
+    precio: 8500,
+    descripcion: "Pollo grillado, arroz de sushi,\npalta,lechuga, cebolla morada encurtida,\nTomates cherrys, semillas de sesamo.",
     imagen: "./assets/img/salad2.jpg"
-  }
+  },
+  
+  {
+    id: 3,
+    nombre: "Poke Crunchy",
+    precio: 8500,
+    descripcion: "Pollo crujiente, arroz de sushi,\nPalta, Queso crema, repollo morado, zanahoria, cebolla\nde verdeo, tomates cherrys",
+    imagen: "./assets/img/salad2.jpg"
+  },
+
+    {
+    id: 4,
+    nombre: "Poke Veggie",
+    descripcion: "Arroz de sushi, Palta,Zanahoria,repollo\nmorado, Cebolla de verdo,Tomates\ncherrys, Choclo, Queso crema",
+    precio: 7000,
+    imagen: "./assets/img/salad2.jpg"
+  },
+
+
+      {
+    id: 5,
+    nombre: "Poke Vegan",
+    descripcion: "Arroz de sushi, Palta,Garbansos,Cebolla\nmorada encurtida,Lechuga, Tomates\nCherrys, Repollo morado.Zanahoria.Semillas\nde sesamo",
+    precio: 7000,
+    imagen: "./assets/img/salad2.jpg"
+  },
+
+        {
+    id: 6,
+    nombre: "Salsa de Soja",
+    descripcion: "Adicional de salsa de soja",
+    precio: 0,
+    imagen: "./assets/img/soya.jpeg"
+  },
+{
+    id: 7,
+    nombre: "Salsa Buenos Aires",
+    descripcion: "Adicional de salsa Bs As",
+    precio: 0,
+    imagen: "./assets/img/buenosaires.jpeg"
+  },
+{
+    id: 8,
+    nombre: "Paltas",
+    descripcion: "Adicional de palta",
+    precio: 1000,
+    imagen: "./assets/img/palta.jpg"
+  },
+{
+    id: 9,
+    nombre: "Cebolla Encurtida",
+    descripcion: "Adicional de cebolla encurtida",
+    precio: 1000,
+    imagen: "./assets/img/palta.jpg"
+  },
+  {
+    id: 10,
+    nombre: "Queso Crema",
+    descripcion: "Adicional de queso crema",
+    precio: 1000,
+    imagen: "./assets/img/palta.jpg"
+  },
+  {
+    id: 11,
+    nombre: "Salsa Sriracha",
+    descripcion: "Adicional de salsa sriracha",
+    precio: 1000,
+    imagen: "./assets/img/palta.jpg"
+
+  },
+
+  {
+    id: 12,
+    nombre: "Coca Cola comun",
+    descripcion: "Adicional de coca cola comun",
+    precio: 1000,
+    imagen: "./assets/img/cocacomun.jpg"
+  },
+
+    {
+    id: 13,
+    nombre: "Coca Cola zero",
+    descripcion: "Adicional de coca cola zero",
+    precio: 1000,
+    imagen: "./assets/img/cocazero.jpg"
+  },
 ];
+
+
+
+
+
+
